852f1428a5392b665f74facb8de53ba818713c3a
Alessandrosantos215 <117760649+Alessandrosantos215@users.noreply.github.com>
2024-07-05 18:14:13 -0300

    Initial commit

---
+++ class1.py
@@ -0,0 +1,7 @@
+n1 = int(input("digite um valor:"))
+n2 = int(input("digite outro agora:"))
+s = n1 + n2
+
+#print( 'a soma entre',n1,'e',n2,'é'  , s)
+
+print ('a soma entre {} mais {} é igual á {}'.format(n1, n2, s))
