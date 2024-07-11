259c14b7e4d579f89f8d4ec634b1d8efc3ae21d3
Alessandrosantos215 <117760649+Alessandrosantos215@users.noreply.github.com>
2024-07-11 16:13:31 -0300

    06

--- class4.py
+++ class4.py
@@ -248,3 +248,270 @@ import random
 #print(frase[:13])
 
 
+#exercicio0022
+
+#nome =  str(input('Digite seu nome:')).strip()
+#print('Analisando seu nome...')
+#print('Seu nome rm Maiuscula é {}'.format(nome.upper()))
+#print('Seu nome em minuscula é {}'.format(nome.lower()))
+#print('Seu nome tem aotodo {} letras'.format(len(nome) - nome.count(' ')))
+##print('Seu primeiro nome tem {} ltras'.format(nome.find(' ')))
+#separa = nome.split()
+#print('Seu primeiro nome é {}e ele tem {} letras'.format(separa[0], len(separa[0])))
+
+#exercicio0023
+
+#num = int(input('Informe numero:'))
+#n = str(num)
+#print('Analisando o numero: {}'.format(num))
+#print('Unidade: {}'.format(n[3]))
+#print('Dezena: {}'.format(n[2]))
+#print('Centena: {}'.format(n[1]))
+#print('Milhar: {}'.format(n[0]))
+
+
+
+
+#exercicio0024
+
+#cid = str(input('Em qual cidade você nasçeu?')).strip()
+#print(cid[:5].upper() == 'SANTO')
+
+
+
+#exercio0025
+
+#@nome = str(input('Qual é seu nome completo?')).strip()
+#print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
+
+
+#exercicio24
+
+#frase = str(input('Digite uma frase?')).upper().strip()
+#print('A letra A aparece {} vezes na frase'.format(frase.count('A')))
+#print('A primeira letra A apareceu na posição {}'.format(frase.find('A')+1))
+#print('A ultima Letra A apareceu na posição: {}'.format(frase.rfind('A')+1))
+
+
+#exercicio27
+
+#n = str(input('Digite seu nome completo:')).strip()
+#nome= n.split()
+#print('Muito prazer em te conhecer!')
+#print('Seu primeiro nome é {}'.format(nome[0]))
+#print('Seu último nome é {}'.format(nome[len(nome)-1]))
+
+
+
+#exercicio 28
+
+#distância = float(input('Qual a distância da sua viagem?'))
+#if distância <= 200:
+ #   preço = distância * 0.50
+#else:
+#    preço = distância * 0.45
+#preço =  distância * 0.50 if distância <= 200 else distância * 0.45
+#print('E o preço da sua passagem sera de R${:.2f}'.format(preço))
+
+
+
+#desafio29
+
+from datetime import date
+#ano = int(input('Que ano quer analisar?Coloque 0 para analisar o ano atual:'))
+#if ano == 0:
+   # ano = date.today().year
+#if ano % 4 == 0 and ano % 100!= 0 or ano % 400 == 0:
+   # print('O ano{}é  BIXESTO'.format(ano))
+#else:
+    #print('O ano {} NÂO é BISSEXTO'.format (ano))
+
+
+
+#exercicio 33
+#a = int(input('Primeiro valor : '))
+#b = int(input('segundo valor : '))
+#c = int(input('terceiro valor : '))
+#Verificar quem é o menor
+#menor = a
+#if b < a and b < c:
+#    menor = b
+#if c < a and c < b:
+#    menor = c
+#verificar quem é o maior
+#maior = a
+#if b > a and b> c:
+#    maior = b
+#if c > a and c > b:
+#    maior = c
+#print('O mair valor digitado foi : {}'.format(maior))
+#print('O menor valor digitado foi : {}'.format(menor))
+
+#exercicio34
+
+#salário = float(input('Qual é o salário do funcionario? R$'))
+#if salário <= 1250:
+#    novo = salário + (salário * 15/100 )
+#else:
+#    novo = salário + (salário * 10/100 )
+#print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salário, novo))
+
+
+
+#exercicio35
+#print('-='*20)
+#print('Analisador de Triandulos')
+#print('-='*20)
+#r1 = float(input('Primeiro seguimento ; '))
+#r2 = float(input('Segundo seguimento ; '))
+#r3 = float(input('Terceiro seguimento ; '))
+#if r1 < r2 + r3 and r2 < r1 + r3 and r3 <r1 + r2:
+#    print('Os seguimentos acima PODEM  formar triangulo!')
+#else:
+#    print('Os Seguimentos acima NÂO podem formar triandulo')
+
+
+
+
+#Aula0012
+
+#nome  =  str(input('Qual é seu nome?'))
+#if nome == 'Gustavo':
+#    print('Que nome bonito')
+#elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+#   print('Seu nome é bem popular no Brasil')
+#elif nome in 'Ana Claudia Jessica Juliana':
+#    print('Belo nome feminino')
+#else:
+#    print('Seu nome é bem normaal')
+#print ('Tenha um bom dia, {}!'.format(nome))
+
+
+#exercicico36
+#casa = float(input('valor da casa?'))
+#Salário = float(input('Salário do comprador'))
+#anos = int(input('Quantos anos de financiamento?'))
+#prestação = casa / (anos * 12 )
+#minímo = Salário * 30 / 100
+#print('Para pagar uma casa de R${:.2}em {} anos'.format(casa, anos), end='')
+#print('a prestação será de R${:.2f}'.format(prestação))
+#if prestação <= minímo:
+#    print('Emprestimo pode ser CONCEDIDO!')
+#else:
+#    print('Emprestimo negado')
+
+
+#exercicio 37
+
+#num = int(input('Digite um numero inteiro: '))
+#print('''Escolhe uma das bases para a conversão:)
+#[ 1 ] Converter para BINARIO
+#[ 2 ] Converter para OCTAL
+#[ 3 ] Convertar para HEXADECIMAL ''')
+#opção = int(input('Sua opção:'))
+#if opção == 1:
+#    print('{} Convertido para BINÀRIO é igual a {}'.format(num, bin(num)[2:]))
+#elif opção == 2:
+#    print('{}convertido para OCTAL é igual a {}'.format(num, oct(num)[2:]))
+#elif opção == 3:
+#    print('{} convertido para HEXADECIMAL é igual a {}'.format(num , hex(num)[2:]))
+#else:
+#    print('Opção invalida tente novamente')
+
+
+#exercicio 38
+#n1 = int(input('primeiro numero: '))
+#n2 = int(input('segundo numero: '))
+#if n1 > n2:
+#    print('o Primeiro numero é maior')
+#elif n2 > n1:
+#    print('o SEGUNDO numero é maior')
+#elif n1 == n2:
+#    print('os numeros são iguais')
+
+#exercicio 39
+
+#from datetime import date
+#atual = date.today().year
+#nasc = int(input('Ano de nascimento: '))
+#idade = atual - nasc
+#print('Quem nasceu em {} tem {} anos em {}'.format(nasc, idade, atual))
+#if idade == 18:
+#    print('Você tem que se alisar imediatamente!')
+#elif idade < 18 :
+#    saldo = 18 - idade
+#    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+#    ano = atual = saldo
+#    print('Seu alistamento sera em {}'.format(ano))
+#elif idade > 18:
+#    saldo = idade - 18
+#    print('Você já deveria ter se alistado há {} anso'.format(saldo))
+#    ano = atual - saldo
+#    print('Seu alistamento foi em {}'.format(ano))
+
+
+
+#exercicio 40
+
+#nota1 = float(input('Primeira nota: '))
+#nota2 = float(input('Segunda nota :'))
+#m#édia = ( nota1 + nota2) / 2
+#print('Tirando {:.1f} e {:.1f},a média do aluno é {:.1f}'.format(nota1, nota2, média))
+#if  7 > média >= 5:
+#    print('O aluno está em RECUPERAÇÃO')
+#elif média < 5:
+#    print("O aluno está REPROVADO")
+#elif média >= 7 :
+#    print('O aluno esta aprovado')
+
+
+#exercicio 41
+
+#from datetime import date
+#atual = date.today().year
+#nascimento = int(input('Ano de Nascimento:'))
+#idade = atual - nascimento
+#print('O atleta tem {} anos'.format(idade))
+#if idade <= 9:
+#    print('Classificação: MIRIM')
+#elif idade <= 14:
+#    print('Classificação: INFANTIL')
+#elif idade <= 19:
+#    print('Classificação: JUNIOR')
+#elif idade <= 25:
+#    print('Classificação: SÊNIOR')
+#else:
+#    print('Classificação: MASTER')
+
+#r1 = float(input("primeiro segmento: "))
+#r2 = float(input('segundo segmento: '))
+#r3 = float(input('terceiro segmento: '))
+#if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+#    print('Os segmentos acima PODEM FORMAR um triângulo ', end='')
+#    if r1 == r2 == r3:
+#        print('EQUILATERO')
+#    elif r1 != r2 != r3 != r1:
+#        print('ESCALENO')
+#   else:
+#        print('ISOSCELES')
+#else:
+#    print('Os segmentos acima NÃO PODEM FORMAR triângulo')
+
+
+
+
+
+#exercicio 43
+
+peso= float(input('qual é seu peso? (Kg) '))
+altura = float(input('qual é sua altura? (m)'))
+imc = peso / (altura ** 2)
+print('IMC dessa pessoa é de {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Voce está ABAIXO do peso normal')
+elif 25 <= imc < 30:
+    print('Voce esta em SOBREPESO')
+elif 30 <= imc < 40:
+    print('Voce está em OBESIDADE!')
+elif imc >= 40:
+    print('Voce está em OBESIDADE MÓRBIDA, cuidado!')
